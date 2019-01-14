5343ff6eb1f6b4ecc075f79cec4f203d0f4f0da8
Kevmsutton <kevmsutton@gmail.com>
2019-01-14 11:00:27 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -62,13 +62,7 @@ describe('index', () => {
     })
   })
 
-  describe('trueAndFalseButtons', function(){
-    it('selects and returns the true and false buttons in the index.html file', function(){
-      expect(trueAndFalseButtons().length).to.eq(2)
-      expect(trueAndFalseButtons()[0].innerText).to.eq("TRUE")
-      expect(trueAndFalseButtons()[1].innerText).to.eq("FALSE")
-    })
-  })
+
   describe('toggleTrueAndFalseButtons', function(){
     it('changes hidden true and false buttons to displayed', function(){
       trueAndFalseButtons().forEach(function(btn){
